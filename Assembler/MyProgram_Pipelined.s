d67a54aac85bc6c86156984044f54e30bcd05994
Krystian, The wizard <krystian.targonski@student.uclouvain.be>
2025-12-01 20:41:42 +0100

    Lab 5 done

--- Assembler/MyProgram_Pipelined.s
+++ Assembler/MyProgram_Pipelined.s
@@ -18,15 +18,28 @@ main:
 	STR R0, [R0, #0x500]    @ Clear the LED
 	LDR R1, [R0, #0x400]    @ Load value A from SPI transfer in R1
 	LDR R2, [R0, #0x404]    @ Load value B from SPI transfer in R2
-	CMP R1, #0		@ If A=0
+	CMP R1, #0				@ If A=0
 	BEQ GCD_Done    
-	CMP R2, #0		@ If B=0
+	CMP R2, #0				@ If B=0
 	BEQ GCD_Done   
 
 GCD_Loop       
-        B GCD_Done              @ YOU NEED TO MODIFY THIS SECTION
+	CMP R1, R2 				@ Compare A and B
+	BEQ GCD_Done
+	BLT Sub_A
+	BGT Sub_B
+	B GCD_Done              @ Go Back to done if nothing is taken
     
-GCD_Done		        
-        STR R1, [R0, #0x408] 	@ CGD=R1=R2, send back through SPI
-  			 	@ Display the GCD on the LEDs, YOU NEED TO ADD AN INSTRUCTION
+Sub_A
+	SUB R2, R2, R1			@ B = B - A 
+	B GCD_Loop
+
+Sub_B
+	SUB R1, R1, R2			@ A = A - B
+	B GCD_Loop
+
+GCD_Done
+	STR R1, [R0, #0x500]		        
+	STR R1, [R0, #0x408] 	@ CGD=R1=R2, send back through SPI
+	@ Display the GCD on the LEDs, YOU NEED TO ADD AN INSTRUCTION
 .end     @ end of code
